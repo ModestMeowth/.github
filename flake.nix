3e889f365b44b7f5dc726010d34b532f1a7cac15
ModestMeowth <email@example.com>
2024-10-11 12:01:05 -0500

    remove keys

--- flake.nix
+++ flake.nix
@@ -2,9 +2,6 @@
   inputs.dotfiles.url = "github:ModestMeowth/dotfiles";
   inputs.dotfiles.flake = false;
 
-  inputs.keys.url = "https://github.com/ModestMeowth.keys";
-  inputs.keys.flake = false;
-
   outputs = { ... } @ inputs: {
     dotfiles = "${inputs.dotfiles}";
     keys = "${inputs.keys}";
